8ee1336ec37080c4426b503bb9c73f659612352a
Jaime Ortiz <jaalorsa517@hotmail>
2020-10-02 20:38:32 -0500

    Se completó el proyecto

--- admin.py
+++ admin.py
@@ -17,30 +17,34 @@ def page_not_found(error):
 @app.route('/admin')
 @login_required
 def admin():
-    users= User.getUsers()
+    users = User.getUsers()
     form_users = TableForm()
     for row in users:
         _row = RowForm()
         _row.nickname = row['name']
         _row.check = bool(int(row['role']))
         form_users.rows.append_entry(_row)
-    context = dict(users = users, form_user = form_users)
-    return render_template('admin.html',**context)
+    context = dict(users=users, form_user=form_users)
+    return render_template('admin.html', **context)
 
-@app.route('/admin', methods =['POST'])
+
+@app.route('/admin', methods=['POST'])
 def adminPost():
-    table_form = TableForm()
-    # import pdb; pdb.set_trace()
+    table_form = TableForm(formdata=request.form)
     if table_form.validate_on_submit():
-        print(table_form.rows.data)
+        rows = [ dict(name=dic['nickname'], role=(str(1) if dic['check'] else str(0))) for dic in table_form.rows.data]
+        User.updateUser(rows)
+        flash('Actualización correcta')
     return redirect(url_for('admin'))
+
 
 @app.route('/')
 @login_required
 def hello():
     name = current_user.id.capitalize()
     flash(f'Bienvenido {name}')
-    return render_template('index.html')
+    admin = bool(int(current_user.role))
+    return render_template('index.html', admin=admin)
 
 
 if __name__ == "__main__":
--- models/users.py
+++ models/users.py
@@ -24,7 +24,7 @@ class User(UserMixin):
     def getUsers(cls):
         with open(cls.path_csv) as csv_file:
             reader = csv.DictReader(csv_file, cls.fieldnames)
-            return [dic for x,dic in enumerate(reader) if x>0]
+            return [dic for x, dic in enumerate(reader) if x > 0]
         return None
 
     @staticmethod
@@ -33,3 +33,24 @@ class User(UserMixin):
         with open(User.path_csv, 'a') as file:
             writer = csv.DictWriter(file, User.fieldnames)
             writer.writerow(user)
+
+    @classmethod
+    def updateUser(cls, list_dic):
+        '''
+        :param list list_dic: Lista de diccionario con clave 'name', 'role'
+        '''
+        root = {}
+        with open(cls.path_csv) as csv_file:
+            reader = csv.DictReader(csv_file, cls.fieldnames)
+            x = 0
+            for row in reader:
+                if (row['name'] == 'root'):
+                    root = row
+                elif (list_dic[x]['name'] == row['name']):
+                    list_dic[x]['password'] = row['password']
+                    x += 1
+        with open(cls.path_csv, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, cls.fieldnames)
+            writer.writeheader()
+            writer.writerow(root)
+            writer.writerows(list_dic)
